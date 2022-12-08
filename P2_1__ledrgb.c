e99d063c147ed676c63db8675972b013fabf28ca
Pincheirapatagonia <53890076+Pincheirapatagonia@users.noreply.github.com>
2022-12-08 16:04:40 -0300

    sd code added

--- P2_1__ledrgb.c
+++ P2_1__ledrgb.c
@@ -10,6 +10,31 @@
 #include "xil_exception.h"
 #include "xil_printf.h"
 #include "xtime_l.h"
+#include "xsysmon.h"
+#include "xstatus.h"
+#include "sleep.h"
+#include "sdCard.h"
+
+
+
+//------- SD param--------
+
+#define MAX_LOG_NUM 20
+
+FIL *fptr;
+
+static void SysMonInterruptHandler(void *CallBackRef);
+static int SysMonSetupInterruptSystem(XScuGic *IntcInstancePtr,
+                                      XSysMon *SysMonPtr,
+                                      u16 IntrId);
+int SysMonFractionToInt(float FloatNum);
+int logNum = 0;
+
+char dataBuffer[1024];
+char *dataPntr = dataBuffer;
+
+XSysMon SysMonInst;          /* System Monitor driver instance */
+XScuGic InterruptController; /* Instance of the XIntc driver. */
 
 //#define MY_PWM XPAR_MY_PWM_CORE_0_S00_AXI_BASEADDR //Because of a bug in Vivado 2015.3 and 2015.4, this value is not correct.
 #define MY_PWM 0x40004000  //This value is found in the Address editor tab in Vivado (next to Diagram tab)
@@ -62,106 +87,158 @@ int main(){
 
 	init_platform();
 
+    //--------Init SD----------
+    int Status;
+    XSysMon_Config *ConfigPtr;
+    /*
+     * Initialize the SysMon driver.
+     */
+    Status = SD_Init();
 
-	    int status;
-	    //----------------------------------------------------
-	    // INITIALIZE THE PERIPHERALS & SET DIRECTIONS OF GPIO
-	    //----------------------------------------------------
+    if (Status != XST_SUCCESS)
+        xil_printf("SD card init failed");
 
-	    // Initialize Switches
-	    status = XGpio_Initialize(&SWInst, SWS_DEVICE_ID);
-	    if (status != XST_SUCCESS)
-	        return XST_FAILURE;
+    fptr = openFile("logData.csv", 'a');
 
+    if (fptr == 0)
+        printf("File opening failed\n\r");
 
+    ConfigPtr = XSysMon_LookupConfig(XPAR_SYSMON_0_DEVICE_ID);
+    if (ConfigPtr == NULL)
+    {
+        return XST_FAILURE;
+    }
+    XSysMon_CfgInitialize(&SysMonInst, ConfigPtr, ConfigPtr->BaseAddress);
+    /*
+     * Set the sequencer in Single channel mode.
+     */
+    XSysMon_SetSequencerMode(&SysMonInst, XSM_SEQ_MODE_SINGCHAN);
+    /*
+     * Set the configuration registers for single channel continuous mode
+     * of operation for the Temperature channel.
+     */
+    Status = XSysMon_SetSingleChParams(&SysMonInst, XSM_CH_TEMP,
+                                       FALSE, FALSE, FALSE);
+    if (Status != XST_SUCCESS)
+    {
+        return XST_FAILURE;
+    }
+    /*
+     * Setup the interrupt system.
+     */
+    Status = SysMonSetupInterruptSystem(&InterruptController,
+                                        &SysMonInst, XPAR_FABRIC_XADC_WIZ_0_IP2INTC_IRPT_INTR);
+    if (Status != XST_SUCCESS)
+    {
+        return XST_FAILURE;
+    }
+    /*
+     * Enable EOC interrupt
+     */
+    XSysMon_IntrEnable(&SysMonInst, XSM_IPIXR_EOC_MASK);
+    /*
+     * Enable global interrupt of System Monitor.
+     */
+    XSysMon_IntrGlobalEnable(&SysMonInst);
 
-	    // Set all switches direction to inputs
-	    XGpio_SetDataDirection(&SWInst, 1, 0xFF);
+    
+    //----------------------------------------------------
+    // INITIALIZE THE PERIPHERALS & SET DIRECTIONS OF GPIO
+    //----------------------------------------------------
 
-	    // SETUP THE TIMER
-	    //----------------------------------------------------
-	    status = XTmrCtr_Initialize(&TMRInst, TMR_DEVICE_ID);
-	    if (status != XST_SUCCESS)
-	        return XST_FAILURE;
-	    XTmrCtr_SetHandler(&TMRInst, (XTmrCtr_Handler)TMR_Intr_Handler, &TMRInst);
-	    // el valor de reset es del cual empieza a contar hasta los 32 bits a una frecuencia de 50MHz
-	    XTmrCtr_SetResetValue(&TMRInst, 0, TMR_LOAD);                                  // setea el valor del reset llamado TMR_LOAD
-	    XTmrCtr_SetOptions(&TMRInst, 0, XTC_INT_MODE_OPTION | XTC_AUTO_RELOAD_OPTION); // setea opciones
+    // Initialize Switches
+    status = XGpio_Initialize(&SWInst, SWS_DEVICE_ID);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
 
+    // Set all switches direction to inputs
+    XGpio_SetDataDirection(&SWInst, 1, 0xFF);
 
-	    // FIN PEDAZO CODIGO TIMER EN MAIN
+    // SETUP THE TIMER
+    //----------------------------------------------------
+    status = XTmrCtr_Initialize(&TMRInst, TMR_DEVICE_ID);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
+    XTmrCtr_SetHandler(&TMRInst, (XTmrCtr_Handler)TMR_Intr_Handler, &TMRInst);
+    // el valor de reset es del cual empieza a contar hasta los 32 bits a una frecuencia de 50MHz
+    XTmrCtr_SetResetValue(&TMRInst, 0, TMR_LOAD);                                  // setea el valor del reset llamado TMR_LOAD
+    XTmrCtr_SetOptions(&TMRInst, 0, XTC_INT_MODE_OPTION | XTC_AUTO_RELOAD_OPTION); // setea opciones
 
+    // FIN PEDAZO CODIGO TIMER EN MAIN
 
-	    // Initialize interrupt controller de los sw
-	    status = IntcInitFunction3(INTC_DEVICE_ID, &SWInst);
-	    if (status != XST_SUCCESS)
-	        return XST_FAILURE;
+    // Initialize interrupt controller de los sw
+    status = IntcInitFunction3(INTC_DEVICE_ID, &SWInst);
+    if (status != XST_SUCCESS)
+        return XST_FAILURE;
 
-int num=0;
-int i;
+    int num = 0;
+    int i;
 
-while(1){
-if(num == 1024){
-num = 0;
-aux ++;
-if(aux> 4){
-	aux =0;
-}}
-num++;
-xil_printf("%d", aux);
+    while (1)
+    {
+        if (num == 1024)
+        {
+            num = 0;
+            aux++;
+            if (aux > 4)
+            {
+                aux = 0;
+            }
+        }
+        num++;
+        xil_printf("%d", aux);
 
-switch(sw_value){
-case 0:
-	Xil_Out32(MY_PWM, 0);
-	Xil_Out32(MY_PWM+4, 0);
-	Xil_Out32(MY_PWM+8, 0);
-	break;
-case 1:
-	Xil_Out32(MY_PWM, 4*255);
-	Xil_Out32(MY_PWM+4, 0);
-	Xil_Out32(MY_PWM+8, 0);
-	break;
-case 2:
-	Xil_Out32(MY_PWM, 0);
-	Xil_Out32(MY_PWM+4, 4*255);
-	Xil_Out32(MY_PWM+8, 0);
-	break;
-case 3:
-	Xil_Out32(MY_PWM,  4*255);
-	Xil_Out32(MY_PWM+4, 4*255);
-	Xil_Out32(MY_PWM+8, 0);
-	break;
-case 4:
-	Xil_Out32(MY_PWM, 0);
-		Xil_Out32(MY_PWM+4, 0);
-		Xil_Out32(MY_PWM+8, 4*255);
-		break;
-case 5:
-	Xil_Out32(MY_PWM, 4*255);
-		Xil_Out32(MY_PWM+4, 0);
-		Xil_Out32(MY_PWM+8, 4*255);
-		break;
-case 6:
-	Xil_Out32(MY_PWM, 0);
-		Xil_Out32(MY_PWM+4, 4*255);
-		Xil_Out32(MY_PWM+8, 4*255);
-		break;
+        switch (sw_value)
+        {
+        case 0:
+            Xil_Out32(MY_PWM, 0);
+            Xil_Out32(MY_PWM + 4, 0);
+            Xil_Out32(MY_PWM + 8, 0);
+            break;
+        case 1:
+            Xil_Out32(MY_PWM, 4 * 255);
+            Xil_Out32(MY_PWM + 4, 0);
+            Xil_Out32(MY_PWM + 8, 0);
+            break;
+        case 2:
+            Xil_Out32(MY_PWM, 0);
+            Xil_Out32(MY_PWM + 4, 4 * 255);
+            Xil_Out32(MY_PWM + 8, 0);
+            break;
+        case 3:
+            Xil_Out32(MY_PWM, 4 * 255);
+            Xil_Out32(MY_PWM + 4, 4 * 255);
+            Xil_Out32(MY_PWM + 8, 0);
+            break;
+        case 4:
+            Xil_Out32(MY_PWM, 0);
+            Xil_Out32(MY_PWM + 4, 0);
+            Xil_Out32(MY_PWM + 8, 4 * 255);
+            break;
+        case 5:
+            Xil_Out32(MY_PWM, 4 * 255);
+            Xil_Out32(MY_PWM + 4, 0);
+            Xil_Out32(MY_PWM + 8, 4 * 255);
+            break;
+        case 6:
+            Xil_Out32(MY_PWM, 0);
+            Xil_Out32(MY_PWM + 4, 4 * 255);
+            Xil_Out32(MY_PWM + 8, 4 * 255);
+            break;
 
-case 7:
-	Xil_Out32(MY_PWM, 4*255);
-		Xil_Out32(MY_PWM+4, 4*255);
-		Xil_Out32(MY_PWM+8, 4*255);
-		break;
-default:
-	Xil_Out32(MY_PWM, 0);
-	Xil_Out32(MY_PWM+4, 0);
-	Xil_Out32(MY_PWM+8, 0);
-	break;
-}
+        case 7:
+            Xil_Out32(MY_PWM, 4 * 255);
+            Xil_Out32(MY_PWM + 4, 4 * 255);
+            Xil_Out32(MY_PWM + 8, 4 * 255);
+            break;
+        default:
+            Xil_Out32(MY_PWM, 0);
+            Xil_Out32(MY_PWM + 4, 0);
+            Xil_Out32(MY_PWM + 8, 0);
+            break;
+        }
 
-
-
-cleanup_platform();
+        cleanup_platform();
 }}
 
 //Fin del main
@@ -272,6 +349,85 @@ int IntcInitFunction(u16 DeviceId, XTmrCtr *TmrInstancePtr)
 
         // Enable GPIO and timer interrupts in the controller
         XScuGic_Enable(&INTCInst, INTCS_GPIO_INTERRUPT_ID);
+
+        return XST_SUCCESS;
+    }
+    static void SysMonInterruptHandler(void *CallBackRef)
+    {
+        u32 IntrStatusValue;
+        u16 TempRawData;
+        float TempData;
+        XSysMon *SysMonPtr = (XSysMon *)CallBackRef;
+        /*
+         * Get the interrupt status from the device and check the value.
+         */
+        XSysMon_IntrGlobalDisable(&SysMonInst);
+        IntrStatusValue = XSysMon_IntrGetStatus(SysMonPtr);
+        XSysMon_IntrClear(SysMonPtr, IntrStatusValue);
+        logNum++;
+
+        if (IntrStatusValue & XSM_IPIXR_EOC_MASK)
+        {
+            TempRawData = XSysMon_GetAdcData(&SysMonInst, XSM_CH_TEMP);
+            TempData = XSysMon_RawToTemperature(TempRawData);
+            printf("\r\nThe Current Temperature is %0.3f Centigrades. %d\r\n", TempData, logNum);
+            sprintf(dataPntr, "%0.3f\n", TempData);
+            dataPntr = dataPntr + 8;
+
+            if (logNum % 10 == 0)
+            {
+            xil_printf("Updating SD card...\n\r");
+            writeFile(fptr, 80, (u32)dataBuffer);
+            dataPntr = (char *)dataBuffer;
+            }
+
+            if (logNum == MAX_LOG_NUM)
+            {
+            closeFile(fptr);
+            SD_Eject();
+            xil_printf("Safe to remove SD Card...\n\r");
+            XSysMon_IntrGlobalDisable(&SysMonInst);
+            }
+            else
+            {
+            sleep(1);
+            XSysMon_IntrGlobalEnable(&SysMonInst);
+            }
+        }
+    }
+
+    static int SysMonSetupInterruptSystem(XScuGic *IntcInstancePtr,
+                                          XSysMon *SysMonPtr,
+                                          u16 IntrId)
+    {
+        int Status;
+        XScuGic_Config *IntcConfig;
+        IntcConfig = XScuGic_LookupConfig(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+        if (NULL == IntcConfig)
+        {
+            return XST_FAILURE;
+        }
+
+        Status = XScuGic_CfgInitialize(IntcInstancePtr, IntcConfig,
+                                       IntcConfig->CpuBaseAddress);
+        if (Status != XST_SUCCESS)
+        {
+            return XST_FAILURE;
+        }
+        XScuGic_SetPriorityTriggerType(IntcInstancePtr, IntrId,
+                                       0xA0, 0x3);
+        Status = XScuGic_Connect(IntcInstancePtr, IntrId,
+                                 (Xil_ExceptionHandler)SysMonInterruptHandler,
+                                 SysMonPtr);
+        if (Status != XST_SUCCESS)
+        {
+            return Status;
+        }
+        XScuGic_Enable(IntcInstancePtr, IntrId);
+
+        Xil_ExceptionInit();
+        Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XScuGic_InterruptHandler, (void *)IntcInstancePtr);
+        Xil_ExceptionEnable();
 
         return XST_SUCCESS;
     }
